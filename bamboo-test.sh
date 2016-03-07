10092d0a0d649503ae54abebcd3dbceefc93f6a8
Morris Brodersen <morris.brodersen.cf@md.de>
2016-03-07 16:24:51 +0100

    fix xunit

--- bamboo-test.sh
+++ bamboo-test.sh
@@ -5,4 +5,4 @@ bin/edge $BAMBOO_ACCOUNT config set user $BAMBOO_USER
 bin/edge $BAMBOO_ACCOUNT config set password $BAMBOO_PASSWORD
 bin/edge $BAMBOO_ACCOUNT config set org $BAMBOO_ORG
 bin/edge $BAMBOO_ACCOUNT config set env $BAMBOO_ENV
-EDGE_CLI_TEST=$BAMBOO_ACCOUNT node node_modules/istanbul/lib/cli.js cover node_modules/mocha/bin/_mocha test -- -R xunit > reports/TEST-all.xml
+EDGE_CLI_TEST=$BAMBOO_ACCOUNT XUNIT_FILE=reports/TEST-all.xml node node_modules/istanbul/lib/cli.js cover node_modules/mocha/bin/_mocha test -- -R xunit-file
