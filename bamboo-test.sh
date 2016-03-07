23abcd7af5d492026329180aae711f1333fa1f5c
Morris Brodersen <morris.brodersen.cf@md.de>
2016-03-07 16:55:46 +0100

    lowercase vars. dammit

--- bamboo-test.sh
+++ bamboo-test.sh
@@ -1,8 +1,8 @@
 npm install
 mkdir -p reports
-EDGE_CLI_CONF=./.edge node bin/edge $BAMBOO_ACCOUNT config set url $BAMBOO_URL
-EDGE_CLI_CONF=./.edge node bin/edge $BAMBOO_ACCOUNT config set user $BAMBOO_USER
-EDGE_CLI_CONF=./.edge node bin/edge $BAMBOO_ACCOUNT config set password $BAMBOO_PASSWORD
-EDGE_CLI_CONF=./.edge node bin/edge $BAMBOO_ACCOUNT config set org $BAMBOO_ORG
-EDGE_CLI_CONF=./.edge node bin/edge $BAMBOO_ACCOUNT config set env $BAMBOO_ENV
-EDGE_CLI_CONF=./.edge EDGE_CLI_TEST=$BAMBOO_ACCOUNT XUNIT_FILE=reports/TEST-all.xml node node_modules/istanbul/lib/cli.js cover node_modules/mocha/bin/_mocha test -- -R xunit-file
+EDGE_CLI_CONF=./.edge node bin/edge $bamboo_account config set url $bamboo_url
+EDGE_CLI_CONF=./.edge node bin/edge $bamboo_account config set user $bamboo_user
+EDGE_CLI_CONF=./.edge node bin/edge $bamboo_account config set password $bamboo_password
+EDGE_CLI_CONF=./.edge node bin/edge $bamboo_account config set org $bamboo_org
+EDGE_CLI_CONF=./.edge node bin/edge $bamboo_account config set env $bamboo_env
+EDGE_CLI_CONF=./.edge EDGE_CLI_TEST=$bamboo_account XUNIT_FILE=reports/TEST-all.xml node node_modules/istanbul/lib/cli.js cover node_modules/mocha/bin/_mocha test -- -R xunit-file
