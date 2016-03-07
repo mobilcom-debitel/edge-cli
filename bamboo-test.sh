0be3a5c4b2b41ae12a83fd87619bb54141cd7c17
Morris Brodersen <morris.brodersen.cf@md.de>
2016-03-07 16:32:47 +0100

    fix bamboo conf

--- bamboo-test.sh
+++ bamboo-test.sh
@@ -1,8 +1,8 @@
 npm install
 mkdir -p reports
-bin/edge $BAMBOO_ACCOUNT config set url $BAMBOO_URL
-bin/edge $BAMBOO_ACCOUNT config set user $BAMBOO_USER
-bin/edge $BAMBOO_ACCOUNT config set password $BAMBOO_PASSWORD
-bin/edge $BAMBOO_ACCOUNT config set org $BAMBOO_ORG
-bin/edge $BAMBOO_ACCOUNT config set env $BAMBOO_ENV
+EDGE_CLI_CONF=.edge bin/edge $BAMBOO_ACCOUNT config set url $BAMBOO_URL
+EDGE_CLI_CONF=.edge bin/edge $BAMBOO_ACCOUNT config set user $BAMBOO_USER
+EDGE_CLI_CONF=.edge bin/edge $BAMBOO_ACCOUNT config set password $BAMBOO_PASSWORD
+EDGE_CLI_CONF=.edge bin/edge $BAMBOO_ACCOUNT config set org $BAMBOO_ORG
+EDGE_CLI_CONF=.edge bin/edge $BAMBOO_ACCOUNT config set env $BAMBOO_ENV
 EDGE_CLI_TEST=$BAMBOO_ACCOUNT XUNIT_FILE=reports/TEST-all.xml node node_modules/istanbul/lib/cli.js cover node_modules/mocha/bin/_mocha test -- -R xunit-file
