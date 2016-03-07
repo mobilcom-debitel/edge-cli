22967302a31250439081f1fae2e90d0d4e732ada
Morris Brodersen <morris.brodersen.cf@md.de>
2016-03-07 16:36:36 +0100

    fix bamboo test

--- bamboo-test.sh
+++ bamboo-test.sh
@@ -5,4 +5,4 @@ EDGE_CLI_CONF=.edge bin/edge $BAMBOO_ACCOUNT config set user $BAMBOO_USER
 EDGE_CLI_CONF=.edge bin/edge $BAMBOO_ACCOUNT config set password $BAMBOO_PASSWORD
 EDGE_CLI_CONF=.edge bin/edge $BAMBOO_ACCOUNT config set org $BAMBOO_ORG
 EDGE_CLI_CONF=.edge bin/edge $BAMBOO_ACCOUNT config set env $BAMBOO_ENV
-EDGE_CLI_TEST=$BAMBOO_ACCOUNT XUNIT_FILE=reports/TEST-all.xml node node_modules/istanbul/lib/cli.js cover node_modules/mocha/bin/_mocha test -- -R xunit-file
+EDGE_CLI_CONF=.edge EDGE_CLI_TEST=$BAMBOO_ACCOUNT XUNIT_FILE=reports/TEST-all.xml node node_modules/istanbul/lib/cli.js cover node_modules/mocha/bin/_mocha test -- -R xunit-file
