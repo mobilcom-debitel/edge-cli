d2b6d65f11e5552d08faa49311c7d320155578f5
Morris Brodersen <morris.brodersen.cf@md.de>
2016-03-14 13:05:25 +0100

    refactor zipping and streaming

--- test/proxy.js
+++ test/proxy.js
@@ -6,15 +6,15 @@ require( './suite' );
 
 describe( 'The proxy controller', function () {
 
-  this.timeout( 20000 );
+  this.timeout( 8000 );
 
   it( 'should report an error when the proxy directory is invalid (deploy)', function ( done ) {
 
     cli.dispatch( [ 'edge', describe.account, 'proxy', 'deploy', 'unknown', 'edge_cli_test' ] )
       .then( describe.not( done ), function ( err ) {
-        assert.ok( err.message.match( /Invalid proxy directory/ ) );
-      } )
-      .then( done, done );
+        assert.ok( err.message.match( /ENOENT/ ) );
+        done();
+      } );
 
   } );
 
@@ -23,9 +23,9 @@ describe( 'The proxy controller', function () {
     cli.dispatch( [ 'edge', describe.account, 'proxy', 'update', 'unknown', 'edge_cli_test' ] )
       .then( describe.not( done ), function ( err ) {
         debug( err.message );
-        assert.ok( err.message.match( /Invalid proxy directory/ ) );
-      } )
-      .then( done, done );
+        assert.ok( err.message.match( /ENOENT/ ) );
+        done();
+      } );
 
   } );
 
